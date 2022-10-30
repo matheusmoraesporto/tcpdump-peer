292888e82f8b33498863969ccf803fd293180ef4
matheusmoraesporto <matheusmoraesporto99@gmail.com>
2022-10-30 22:57:47 +0000

    just sent one message

--- tcp/client.go
+++ tcp/client.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
 func (_ ConnectionTCP) RunClient(ipLocal, ipRemote string, portLocal, portRemote int) {
@@ -16,24 +15,11 @@ func (_ ConnectionTCP) RunClient(ipLocal, ipRemote string, portLocal, portRemote
 		return
 	}
 
-	for {
-		// lendo entrada do terminal
-		leitor := bufio.NewReader(os.Stdin)
-		fmt.Print("texto a ser enviado: ")
-		texto, erro2 := leitor.ReadString('\n')
-		if erro2 != nil {
-			fmt.Println(erro2)
-		}
+	// for {
+	// escrevendo a mensagem na conexão (socket)
+	fmt.Fprintf(connection, fmt.Sprintf("teste %s\n", localAddr.String()))
 
-		// escrevendo a mensagem na conexão (socket)
-		fmt.Fprintf(connection, texto+"\n")
-
-		// ouvindo a resposta do servidor (eco)
-		bufio.NewReader(connection).ReadString('\n')
-		// if err3 != nil {
-		// 	fmt.Println(err3)
-		// }
-		// // escrevendo a resposta do servidor no terminal
-		// fmt.Print("Resposta do servidor: " + mensagem)
-	}
+	// ouvindo a resposta do servidor (eco)
+	bufio.NewReader(connection).ReadString('\n')
+	// }
 }
