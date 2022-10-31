8ebe3f20eff7a60752354452c78ef9cd1033f382
matheusmoraesporto <matheusmoraesporto99@gmail.com>
2022-10-31 21:47:42 +0000

    Revert "retry connection"
    This reverts commit f1ea90fcc6e9940fdd61b6ff614646597466b5e8.

--- tcp/client.go
+++ tcp/client.go
@@ -1,20 +1,18 @@
 package tcp
 
 import (
-	"errors"
 	"fmt"
 	"net"
-	"time"
 )
-
-const msgAddressAlreadyInUse = "bind: address already in use"
 
 func (_ ConnectionTCP) RunClient(ipLocal, ipRemote string, portLocal, portRemote int) {
 	localAddr := HandleTCPAddress(ipLocal, portLocal)
 	remoteAddr := HandleTCPAddress(ipRemote, portRemote)
 
-	connection, err := retryConnection(localAddr, remoteAddr)
+	connection, err := net.DialTCP("tcp", localAddr, remoteAddr)
 	if err != nil {
+		fmt.Println("deu erro aqui")
+		fmt.Println(err)
 		return
 	}
 
@@ -27,32 +25,4 @@ func (_ ConnectionTCP) RunClient(ipLocal, ipRemote string, portLocal, portRemote
 	// ouvindo a resposta do servidor (eco)
 	// bufio.NewReader(connection) //.ReadString('\n')
 	// }
-}
-
-func retryConnection(localAddr *net.TCPAddr, remoteAddr *net.TCPAddr) (*net.TCPConn, error) {
-	retryfunc := func() (*net.TCPConn, error) {
-		connection, err := net.DialTCP("tcp", localAddr, remoteAddr)
-		if err != nil {
-			fmt.Println("deu erro aqui")
-			fmt.Println(err)
-			return nil, err
-		}
-
-		return connection, err
-	}
-
-	retryPeriod := time.Second * 10
-	timeout := time.After(time.Second * 45)
-	for {
-		select {
-		case <-timeout:
-			connection, err := retryfunc()
-
-			if err == nil {
-				return connection, err
-			}
-			return nil, errors.New("Máxima tentativa de conxões atingidas")
-		case <-time.After(retryPeriod):
-		}
-	}
 }
