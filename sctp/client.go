8f633f146993be10b74b399f371bb3a90bac90be
matheusmoraesporto <matheusmoraesporto99@gmail.com>
2022-10-31 22:54:12 +0000

    add errors on client side to sctp

--- sctp/client.go
+++ sctp/client.go
@@ -10,21 +10,21 @@ func (_ ConnectionSCTP) RunClient(ipLocal, ipRemote string, portLocal, portRemot
 	addr := fmt.Sprintf("%s:%d", ipLocal, portLocal)
 	clientAddr, err := sctp.MakeSCTPAddr(SCTPNetowrk, addr)
 	if err != nil {
-		fmt.Println("Erro:", err)
+		fmt.Println("Erro: -> MakeSCTPAddr", err)
 		return
 	}
 
 	addr = fmt.Sprintf("%s:%d", ipRemote, portRemote)
 	serverAddr, err := sctp.MakeSCTPAddr(SCTPNetowrk, addr)
 	if err != nil {
-		fmt.Println("Erro:", err)
+		fmt.Println("Erro: -> MakeSCTPAddr", err)
 		return
 	}
 
 	initMsg := NewSCTPInitMessage()
 	conn, err := sctp.DialSCTP(SCTPNetowrk, clientAddr, serverAddr, &initMsg)
 	if err != nil {
-		fmt.Println("Erro:", err)
+		fmt.Println("Erro -> DialSCTP:", err)
 		return
 	}
 	defer conn.Close()
@@ -36,6 +36,6 @@ func sendMessageToServer(conn *sctp.SCTPConn) {
 	msg := fmt.Sprintf("Oi eu sou %s", conn.LocalAddr().String())
 	_, err := conn.SendMsg([]byte(msg), nil)
 	if err != nil {
-		fmt.Println("Erro:", err)
+		fmt.Println("Erro: -> sendMessageToServer", err)
 	}
 }
