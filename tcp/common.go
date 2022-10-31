c006d6f7934805997ce25827828a4e1f2eb42686
matheusmoraesporto <matheusmoraesporto99@gmail.com>
2022-10-31 21:55:33 +0000

    try random client port

--- tcp/common.go
+++ tcp/common.go
@@ -5,6 +5,12 @@ import "net"
 type ConnectionTCP struct{}
 
 func HandleTCPAddress(ip string, port int) *net.TCPAddr {
+	if port == 0 {
+		return &net.TCPAddr{
+			IP: net.ParseIP(ip),
+		}
+	}
+
 	return &net.TCPAddr{
 		Port: port,
 		IP:   net.ParseIP(ip),
